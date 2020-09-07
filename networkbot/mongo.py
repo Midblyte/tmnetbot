7c3c9e1f64c65faabc735587e46fd3de0e2c5570
mario <marioanacl22@gmail.com>
2020-09-07 19:05:21 +0200

    License has been changed to GNU GPL v3+
    Signed-off-by: mario <marioanacl22@gmail.com>

--- networkbot/mongo.py
+++ networkbot/mongo.py
@@ -1,3 +1,21 @@
+# tmnetbot - Telegram bot
+# Copyright (C) 2020 Midblyte <https://github.com/Midblyte>
+#
+# This file is part of tmnetbot.
+#
+# tmnetbot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# tmnetbot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with tmnetbot.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 from typing import Any, Callable, Optional
 
