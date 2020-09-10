c69a922951a24b57aaa1404dd668caeba6f19fb6
mario <marioanacl22@gmail.com>
2020-09-10 18:58:40 +0200

    refactoring and optimizations
    Signed-off-by: mario <marioanacl22@gmail.com>

--- networkbot/handlers/info.py
+++ networkbot/handlers/info.py
@@ -15,22 +15,27 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with tmnetbot.  If not, see <https://www.gnu.org/licenses/>.
-
+from datetime import timedelta
 from typing import Dict, Callable
 
 from pyrogram import filters
-from pyrogram.types import Message, InlineKeyboardMarkup as Keyboard, InlineKeyboardButton as Button, CallbackQuery
+from pyrogram.types import Message, InlineKeyboardMarkup as Keyboard, CallbackQuery
 
-from ..helpers import format_documents_list, can_send_icon, get_documents_range, fmt_time
+from ..utils.channels import can_send_icon
+from ..utils.documents import get_documents_range, format_documents_list
 from ..mongo import channels, options
 from ..telegram import telegram
+from ..utils.keyboards import custom_btn
+from ..utils.time import fmt_time
 
+
+_PREFIX = channels.name
 
 not_an_admin = "\
-Non sei admin di alcun canale"
+Non sei admin di alcun canale."
 
 admin_of_these_channels = "\
-Canali di cui sei admin:\n\n{channels}"
+<b>Canali di cui sei admin</b>:\n\n{channels}"
 
 loading_channels = "\
 Caricamento canali di cui sei amministratore..."
@@ -49,7 +54,7 @@ async def info(_, message: Message):
     await _navigate(await message.reply_text(loading_channels), message.from_user.id)
 
 
-@telegram.on_callback_query(filters.create(lambda _, __, cq: cq.data.startswith(f"{channels.name}_admins_nav_")))
+@telegram.on_callback_query(filters.create(lambda _, __, cq: cq.data.startswith(f"{_PREFIX}_admins_nav_")))
 async def get_channels_info_page(_, callback_query: CallbackQuery):
     user_id, offset = map(int, callback_query.data.rsplit('_', 2)[1:])
 
@@ -58,7 +63,7 @@ async def get_channels_info_page(_, callback_query: CallbackQuery):
     await _navigate(callback_query.message, user_id, offset)
 
 
-@telegram.on_callback_query(filters.create(lambda _, __, cq: cq.data.startswith(f"{channels.name}_rm_")))
+@telegram.on_callback_query(filters.create(lambda _, __, cq: cq.data.startswith(f"{_PREFIX}_rm_")))
 async def get_channels_info_page(_, callback_query: CallbackQuery):
     user_id, channel_id, offset = map(int, callback_query.data.rsplit('_', 3)[1:])
 
@@ -79,16 +84,43 @@ async def _navigate(message: Message, user_id: int, offset=0):
 
     for d in documents:
         if d.get('scheduling').get('in_queue') is True:
-            keyboard.inline_keyboard.append([Button(d.get("name"),
-                                                    f"{channels.name}_rm_{user_id}_{d.get('channel_id')}_{offset}")])
+            keyboard.inline_keyboard.append([custom_btn(d.get("name"), _PREFIX,
+                                                                        ["rm", user_id, d.get("channel_id"), offset])])
 
     if len(keyboard.inline_keyboard) == 0:
         keyboard = None
 
-    fmt_channels = format_documents_list(documents.rewind(), lambda c: f"""\
-{can_send_icon(c.get('last_send'), c.get('delta'), c.get('scheduling').get('in_queue'), options('channels_delta'))} \
-{c.get('name')}
-ID: {c.get('channel_id')}
-Dal: {fmt_time(c.get('_id').generation_time)}""")
+    fmt_channels = format_documents_list(documents.rewind(), _format_channel_info)
 
     await message.edit_text(admin_of_these_channels.format(channels=fmt_channels), reply_markup=keyboard)
+
+
+def _format_channel_info(channel: Dict, detailed=False) -> str:
+    name = channel.get('name')
+    channel_id = channel.get('channel_id')
+    last_send = channel.get('last_send')
+    channel_delta = channel.get('delta')
+    delta = channel_delta or options("channels_delta")
+    in_queue = channel.get('scheduling').get('in_queue')
+    icon = can_send_icon(last_send, delta, in_queue)
+    generation_time = fmt_time(channel.get('_id').generation_time)
+    last_send_time = fmt_time(last_send)
+
+    text = f"""\
+{icon} {name}
+Ultimo invio » {last_send_time}"""
+
+    if detailed:
+        text = f"""\
+{icon} {name} [ID {channel_id}]
+Ultimo invio » {last_send_time}
+Aggiunto il » {generation_time}
+"""
+
+        if icon == '🔇':
+            text += f"\nProssimo invio dal: {fmt_time(last_send + timedelta(seconds=delta))}"
+
+        if channel_delta is not None:
+            text += f"\nDistanziamento personalizzato: {channel_delta} secondi"
+
+    return text
