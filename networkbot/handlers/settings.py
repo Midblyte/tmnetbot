e79ba9fbae933501c49032f4141b9912e81bf981
mario <marioanacl22@gmail.com>
2020-09-11 15:07:46 +0200

    implement settings for network and channels' delta
    Signed-off-by: mario <marioanacl22@gmail.com>

--- networkbot/handlers/settings.py
+++ networkbot/handlers/settings.py
@@ -19,26 +19,35 @@
 from pyrogram import filters
 from pyrogram.types import Message, CallbackQuery, InlineKeyboardMarkup as Keyboard
 
-from .. import filters as custom_filters
-from ..utils.keyboards import select_double_time_keyboard, custom_btn, select_double_time_header, confirm_btn
+from .. import periodic_task, filters as custom_filters
+from ..utils.keyboards import select_double_time_keyboard, custom_btn, select_double_time_header, confirm_btn, \
+    select_time_keyboard
 from ..mongo import options, admins, options_collection
 from ..telegram import telegram
-from ..utils.time import fmt_mins
+from ..utils.time import fmt_mins, fmt_time_duration
 
 _PREFIX = "settings"
 
-get_buttons = lambda: [
+get_settings_menu_buttons = lambda: [
     [custom_btn('Network -> fascia oraria', _PREFIX, ['forward', 'tr', 'set',
                                                       options('time_range_start'), options('time_range_end')])],
     [custom_btn('Network -> distanziamento', _PREFIX, ['forward', 'nd', 'set', options('network_delta')])],
     [custom_btn('Canali -> distanziamento', _PREFIX, ['forward', 'cd', 'set', options('channels_delta')])]
 ]
 
+_DELTA_BUTTONS = ('1s', 1), ('30s', 30), ('15m', 900), ('4h', 14400)
+
 settings_text = "\
 Impostazioni"
 
 select_time_range = "\
-Fascia oraria consentita: {start} - {end}"
+Fascia oraria consentita: <b>{start}</b> - <b>{end}</b>"
+
+select_network_delta = "\
+Distanziamento messaggi nel canale del network: <b>{network_delta}</b>"
+
+select_channels_delta = "\
+Gli amministratori dei canali possono inviare nuovi messaggi ogni: <b>{channels_delta}</b>"
 
 not_allowed = "\
 Non sei amministratore del bot"
@@ -55,12 +64,14 @@ settings_ok = lambda msg: msg.edit_text(text="Salvato",
 
 @telegram.on_message(filters.private & filters.command(["settings", "impostazioni"]) & custom_filters.is_admin)
 async def settings(_, message: Message):
-    await message.reply_text(settings_text, reply_markup=Keyboard(get_buttons()))
+    await message.reply_text(settings_text, reply_markup=Keyboard(get_settings_menu_buttons()))
 
 
 @telegram.on_callback_query(filters.create(lambda _, __, cq: cq.data.startswith(f"{_PREFIX}_menu")))
 async def settings_menu(_, callback_query: CallbackQuery):
-    await callback_query.message.edit_text(settings_text, reply_markup=Keyboard(get_buttons()))
+    await callback_query.answer()
+
+    await callback_query.message.edit_text(settings_text, reply_markup=Keyboard(get_settings_menu_buttons()))
 
 
 @telegram.on_callback_query(filters.create(lambda _, __, cq: cq.data.startswith(f"{_PREFIX}_forward_tr_set_")))
@@ -69,7 +80,9 @@ async def settings_forward_time_range_set(_, callback_query: CallbackQuery):
 
     start, end = map(int, callback_query.data.rsplit('_', 2)[1:])
 
-    time_keyboard = select_double_time_keyboard(f"{_PREFIX}", start, end, callback_query.data.split('_', 5)[1:4])
+    args = callback_query.data.split('_')[1:4]
+
+    time_keyboard = select_double_time_keyboard(_PREFIX, start, end, args)
 
     fmt_text = select_time_range.format(start=fmt_mins(start), end=fmt_mins(end))
 
@@ -98,10 +111,29 @@ async def settings_forward_time_range_confirm(_, callback_query: CallbackQuery):
 async def settings_forward_network_delta_set(_, callback_query: CallbackQuery):
     await callback_query.answer()
 
+    value = int(callback_query.data.rsplit('_', 1)[1])
+
+    args = callback_query.data.split('_')[1:4]
+
+    time_keyboard = select_time_keyboard(_PREFIX, value, args, _DELTA_BUTTONS)
+
+    fmt_text = select_network_delta.format(network_delta=fmt_time_duration(value))
+
+    await callback_query.message.edit_text(fmt_text, reply_markup=Keyboard([
+        *time_keyboard,
+        [confirm_btn(_PREFIX, [*args[:-1], 'confirm', value])]
+    ]))
+
 
 @telegram.on_callback_query(filters.create(lambda _, __, cq: cq.data.startswith(f"{_PREFIX}_forward_nd_confirm_")))
 async def settings_forward_network_delta_confirm(_, callback_query: CallbackQuery):
     await callback_query.answer()
+
+    value = int(callback_query.data.rsplit('_', 1)[1])
+
+    options_collection.find_one_and_update({}, {"$set": {"network_delta": value}})
+
+    periodic_task.restart()
 
     await settings_ok(callback_query.message)
 
@@ -110,9 +142,26 @@ async def settings_forward_network_delta_confirm(_, callback_query: CallbackQuer
 async def settings_forward_channels_delta_set(_, callback_query: CallbackQuery):
     await callback_query.answer()
 
+    value = int(callback_query.data.rsplit('_', 1)[1])
+
+    args = callback_query.data.split('_')[1:4]
+
+    time_keyboard = select_time_keyboard(_PREFIX, value, args, _DELTA_BUTTONS)
+
+    fmt_text = select_channels_delta.format(channels_delta=fmt_time_duration(value))
+
+    await callback_query.message.edit_text(fmt_text, reply_markup=Keyboard([
+        *time_keyboard,
+        [confirm_btn(_PREFIX, [*args[:-1], 'confirm', value])]
+    ]))
+
 
 @telegram.on_callback_query(filters.create(lambda _, __, cq: cq.data.startswith(f"{_PREFIX}_forward_cd_confirm_")))
 async def settings_forward_channels_delta_confirm(_, callback_query: CallbackQuery):
     await callback_query.answer()
+
+    value = int(callback_query.data.rsplit('_', 1)[1])
+
+    options_collection.find_one_and_update({}, {"$set": {"channels_delta": value}})
 
     await settings_ok(callback_query.message)
