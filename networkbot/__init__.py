897e3f487c7c74c9334a518bd1b4beb6f06fe7dc
Mario A <marioanacl22@gmail.com>
2020-09-20 22:27:10 +0200

    update version
    Signed-off-by: Mario A <marioanacl22@gmail.com>

--- networkbot/__init__.py
+++ networkbot/__init__.py
@@ -19,7 +19,7 @@
 from . import mongo, handlers, core, upgrader
 
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __license__ = "GNU General Public License v3 or later (GPLv3+)"
 __copyright__ = "Copyright (C) 2020 Midblyte <https://github.com/Midblyte>"
 
