fc743b6d16ee5c157c24069b7d9791a9aee584c2
Mario A <marioanacl22@gmail.com>
2020-09-20 20:48:40 +0200

    add internationalization
    Signed-off-by: Mario A <marioanacl22@gmail.com>

--- networkbot/handlers/make_me_admin.py
+++ networkbot/handlers/make_me_admin.py
@@ -20,18 +20,16 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from .. import filters as custom_filters
-from ..mongo import admins
+from ..internationalization import translator
+from ..mongo import users
 from ..telegram import telegram
 
 
-makemeadmin_info = """\
-Ora sei admin.
-
-Nota: a partire da questo momento il comando non funzionerà più."""
+_ = translator("makemeadmin")
 
 
 @telegram.on_message(filters.private & filters.command("makemeadmin") & custom_filters.no_admins)
 async def make_me_admin(_, message: Message):
     admins.insert_one({"user_id": message.from_user.id, "name": message.from_user.first_name})
 
-    await message.reply_text(makemeadmin_info)
+    await message.reply_text(_("info", locale=message.from_user.language_code))
