42e4246a87b240dc2ff0a7615cb1e02f9acb3d89
Mario A <marioanacl22@gmail.com>
2020-09-21 10:56:02 +0200

    add internationalization
    Signed-off-by: Mario A <marioanacl22@gmail.com>

--- networkbot/handlers/promote.py
+++ networkbot/handlers/promote.py
@@ -21,37 +21,29 @@ from pyrogram.types import Message
 from pyrogram.errors import RPCError
 
 from .. import filters as custom_filters
+from ..internationalization import translator
 from ..mongo import users
 from ..telegram import telegram
 
 
-usage = "\
-Uso: /promuovi @Username\n\nAlias: /promote"
-
-already_listed_admin = "\
-L'admin è già in lista."
-
-not_an_user = "\
-Non è un utente"
-
-promote_successful = "\
-Aggiunto {} ({}) come amministratore."
+_ = translator("promote")
 
 
 @telegram.on_message(filters.private & filters.command(["promuovi", "promote"]) & custom_filters.is_admin)
 async def promote(client: Client, message: Message):
     if ' ' not in message.text:
-        return await message.reply_text(usage)
+        return await message.reply_text(_("usage", locale=message.from_user.language_code))
 
     user_as_text = message.text.split(' ')[1]
 
     try:
         admin = await client.get_users(user_as_text)
     except RPCError:
-        return await message.reply_text(not_an_user)
+        return await message.reply_text(_("not_an_user", locale=message.from_user.language_code))
 
     if users.find_one({"user_id": admin.id, "admin": True}):
-        return await message.reply_text(already_listed_admin)
+        return await message.reply_text(_("already_in_list", locale=message.from_user.language_code))
 
     users.insert_one({"user_id": admin.id, "name": admin.first_name, "admin": True})
-    await message.reply_text(promote_successful.format(admin.first_name, admin.id))
+    await message.reply_text(_("successfully_promoted", locale=message.from_user.language_code,
+                               mention=admin.mention, first_name=admin.first_name, id=admin.id))
