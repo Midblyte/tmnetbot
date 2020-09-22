69820242b10e7de83535d3b41305741fa92556a1
Mario A <marioanacl22@gmail.com>
2020-09-22 15:28:33 +0200

    remove useless import
    Signed-off-by: Mario A <marioanacl22@gmail.com>

--- networkbot/config.py
+++ networkbot/config.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with tmnetbot.  If not, see <https://www.gnu.org/licenses/>.
 
-import os
 import sys
 from collections import namedtuple
 
