2620c97131fa5a11307f533c228ef36d47ce8a51
Mario A <marioanacl22@gmail.com>
2020-10-11 13:16:44 +0200

    forward handling rewritten from scratch; closes #2
    Signed-off-by: Mario A <marioanacl22@gmail.com>

--- networkbot/handlers/forward.py
+++ networkbot/handlers/forward.py
@@ -16,108 +16,152 @@
 # You should have received a copy of the GNU General Public License
 # along with tmnetbot.  If not, see <https://www.gnu.org/licenses/>.
 
-import functools
-from datetime import datetime, timedelta
-from typing import Optional, List, Dict
+from datetime import datetime as dt, timedelta as td, time as t
+from typing import Optional, Tuple
 
+import pymongo
 from pyrogram import filters
-from pyrogram.errors import RPCError
-from pyrogram.methods.chats.get_chat_members import Filters
-from pyrogram.types import Message, ChatMember, CallbackQuery, InlineKeyboardMarkup as Keyboard
+from pyrogram.types import Message, InlineKeyboardMarkup as Keyboard, InlineKeyboardButton as Button, User, \
+    CallbackQuery
 
 from .. import filters as custom_filters
 from ..internationalization import translator
-from ..utils.time import fmt_time, fmt_mins
-from ..utils.keyboards import select_double_time_keyboard, select_double_time_header, confirm_btn
 from ..mongo import channels, options
 from ..telegram import telegram
+from ..utils.channels import get_status, ChannelSendingState, ChannelState
+from ..utils.general import extract_args, args_joiner
+from ..utils.time import fmt_time, fmt_time_duration, is_forwarding_allowed, MINUTES_PER_DAY
+from ..utils.users import notify
 
 
-_PREFIX = "forward"
+_PREFIX = "forward",
 
-_, _g = translator(_PREFIX), translator("general"),
-
-_path = functools.partial(custom_filters.arguments, _PREFIX)
+_, _g = translator(*_PREFIX), translator("general")
 
 
 @telegram.on_message(filters.private & filters.forwarded)
-async def forward(__, message: Message):
-    locale, tg_channel = message.from_user.language_code, message.forward_from_chat
+def forward(__, message: Message):
+    locale = message.from_user.language_code
 
-    if not tg_channel or tg_channel.type != "channel":
-        return await message.reply_text(_("not_a_channel", locale=locale))
+    sent_message = message.reply_text(_g("loading", locale=locale))
 
-    doc_channel: Optional[Dict] = channels.find_one({"channel_id": tg_channel.id})
-
-    if doc_channel is None:
-        return await message.reply_text(_("not_part_of_the_network", locale=locale))
+    if message.forward_from_chat.type != "channel":
+        return sent_message.edit_text(_("not_a_channel", locale=locale))
 
     try:
-        channel_admins: List[ChatMember] = await tg_channel.get_members(filter=Filters.ADMINISTRATORS)
-    except RPCError:
-        return await message.reply_text(_("im_not_an_admin", locale=locale))
+        status = get_status(message.forward_from_chat.id, message.from_user, tg_channel=message.forward_from_chat)
 
-    if message.from_user.id not in map(lambda a: a.user.id, channel_admins):
-        return await message.reply_text(_("youre_not_an_admin", locale=locale))
+        _check_forwarding_status(status, locale=locale)
+    except ValueError as error:
+        error_string, params = error.args
 
-    last_send: datetime = doc_channel.get("last_send") or datetime.min
+        return sent_message.edit_text(_(error_string, **params, locale=locale))
 
-    delta: int = doc_channel.get("delta") or options("channels_delta")
-
-    diff: timedelta = datetime.utcnow() - last_send
-
-    if diff.total_seconds() < delta:
-        return await message.reply_text(_("not_allowed_until", locale=locale,
-                                          date=fmt_time(last_send + timedelta(seconds=delta))))
-
-    if doc_channel.get("scheduling").get("in_queue"):
-        return await message.reply_text(_("already_in_queue", locale=locale))
-
-    sent_message = await message.reply_text(_g("loading", locale=locale))
-    start, end = options("time_range_start"), options("time_range_end")
-
-    channel_id, msg_id = tg_channel.id, message.forward_from_message_id
-
-    time_keyboard = select_double_time_keyboard(_PREFIX, start, end, ['set', channel_id, msg_id])
-
-    fmt_text = _("select_time_range", locale=locale, **{k: fmt_mins(v) for k, v in {"start":
-                 start, "end": end, "general_start": start, "general_end": end}.items()})
-
-    await sent_message.edit_text(fmt_text, reply_markup=Keyboard([
-        select_double_time_header(),
-        *time_keyboard,
-        [confirm_btn(_PREFIX, ['confirm', channel_id, msg_id, start, end])]
-    ]))
+    _menu(sent_message, message.from_user, message.forward_from_chat.id, message.forward_from_message_id)
 
 
-@telegram.on_callback_query(_path("set"))
-async def select_forward_time(__, callback_query: CallbackQuery):
-    await callback_query.answer()
+@telegram.on_callback_query(custom_filters.arguments(*_PREFIX, "refresh"))
+def refresh(__, callback_query: CallbackQuery):
+    callback_query.answer()
 
-    channel_id, msg_id, start, end = map(int, callback_query.data.rsplit('_', 4)[1:])
+    forward_from_chat_id, forward_from_message_id = extract_args(callback_query.data, 2, int)
 
-    time_keyboard = select_double_time_keyboard(_PREFIX, start, end, ['set', channel_id, msg_id])
-
-    fmt_text = _("select_time_range", locale=callback_query.from_user.language_code,
-                 general_start=options("time_range_start"), general_end=options("time_range_end"),
-                 start=fmt_mins(start), end=fmt_mins(end))
-
-    await callback_query.message.edit_text(fmt_text, reply_markup=Keyboard([
-        select_double_time_header(),
-        *time_keyboard,
-        [confirm_btn(_PREFIX, ['confirm', channel_id, msg_id, start, end])]
-    ]))
+    _menu(callback_query.message, callback_query.from_user, forward_from_chat_id, forward_from_message_id)
 
 
-@telegram.on_callback_query(_path("confirm"))
-async def confirm_forward_time(__, callback_query: CallbackQuery):
-    await callback_query.answer()
+@telegram.on_callback_query(custom_filters.arguments(*_PREFIX, "confirm"))
+def confirm(__, callback_query: CallbackQuery):
+    callback_query.answer()
 
-    channel_id, msg_id, start, end = map(int, callback_query.data.rsplit('_', 4)[1:])
+    locale = callback_query.from_user.language_code
 
-    if channels.find_one_and_update({"channel_id": channel_id, "scheduling.in_queue": False}, {"$set": {"scheduling": {
-            "in_queue": True, "message_id": msg_id, "time_from": start, "time_to": end}}}) is None:
-        return await callback_query.message.edit_text(_("already_in_queue",
-                                                        locale=callback_query.from_user.language_code))
+    forward_from_chat_id, forward_from_message_id = extract_args(callback_query.data, 2, int)
 
-    await callback_query.message.edit_text(_g("ok"))
+    try:
+        status = get_status(forward_from_chat_id, callback_query.from_user)
+
+        _check_forwarding_status(status, locale=locale)
+    except ValueError as error:
+        error_string, params = error.args
+
+        return callback_query.message.edit_text(_(error_string, **params, locale=locale))
+
+    dtnow, network_delta, eta = check_forwarding_time(status.queue_length)
+
+    channels.find_one_and_update({"channel_id": forward_from_chat_id}, {"$set": {"scheduling": {
+        "in_queue": True,
+        "message_id": forward_from_message_id,
+        "inserted_on": dtnow,
+    }}})
+
+    callback_query.message.edit_text(_("successfully_scheduled", locale=locale))
+
+    start_mins, end_mins = options("time_range_start") or 0, options("time_range_end") or MINUTES_PER_DAY
+    start, end = map(lambda minutes: dt.combine(dtnow.date(), t.min) + td(minutes=minutes), (start_mins, end_mins))
+    if eta == 0 and is_forwarding_allowed(start, end, dtnow):
+        from ..scheduler import periodic_task
+
+        periodic_task.restart()
+
+    reply_markup = lambda user_locale: Keyboard([[
+        Button(_("go_to_the_message", locale=user_locale),
+               url=f"https://t.me/c/{-forward_from_chat_id + 10**12}/{forward_from_message_id}")]])
+
+    notify("successfully_scheduled", channel_id=forward_from_chat_id, exception=callback_query.from_user.id,
+           reply_markup=reply_markup)
+
+
+def _menu(message: Message, user: User, forward_from_chat_id: int, forward_from_message_id: int):
+    locale = user.language_code
+
+    try:
+        status = get_status(forward_from_chat_id, user)
+
+        _check_forwarding_status(status, locale=locale)
+    except ValueError as error:
+        error_string, params = error.args
+
+        return message.edit_text(_(error_string, **params, locale=locale))
+
+    params = lambda action: args_joiner(*_PREFIX, action, forward_from_chat_id, forward_from_message_id)
+
+    keyboard = Keyboard([
+        [Button(_g("refresh", locale=locale), params("refresh"))],
+        [Button(_g("confirm", locale=locale), params("confirm"))]
+    ])
+
+    dtnow, network_delta, eta = check_forwarding_time(status.queue_length)
+
+    wait_string = "no_awaiting_time" if eta == 0 else "awaiting_time"
+
+    fmt_text = '\n'.join([
+            _("info", locale=locale),
+            _("queue", count=status.queue_length, locale=locale),
+            _(wait_string, time=fmt_time_duration(eta, locale), locale=locale),
+            _("last_updated_at", time=fmt_time(dt.utcnow(), locale), locale=locale)
+        ])
+
+    message.edit_text(fmt_text, reply_markup=keyboard)
+
+
+def check_forwarding_time(queue_length: int) -> Tuple:
+    # todo: let it be recursive to get the real ETA
+    network_delta = options("network_delta")
+    dtnow = dt.utcnow()
+    last_sent_date: dt = (channels.find_one({}, sort=[("last_send", pymongo.DESCENDING)], limit=1,
+                                            projection=["last_send"]) or {}).get("last_send", dt.min)
+
+    queue_eta = queue_length * network_delta
+    cooldown_eta = max(0, int((last_sent_date + td(seconds=network_delta) - dtnow).total_seconds()))
+    eta = queue_eta + cooldown_eta
+
+    return dtnow, network_delta, eta
+
+
+def _check_forwarding_status(channel_status: ChannelState, locale: Optional[str] = None):
+    if channel_status.state == ChannelSendingState.IN_QUEUE:
+        raise ValueError("already_in_queue", {})
+
+    if channel_status.state == ChannelSendingState.RESTING:
+        raise ValueError("not_allowed_until",
+                         {"date": fmt_time(channel_status.last_send + channel_status.delta, locale)})
