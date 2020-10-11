235d93da069b2c6ff83d621f63ec32a00a6fc1ae
Mario A <marioanacl22@gmail.com>
2020-10-11 13:32:47 +0200

    add keyboard utils; remove old stuff
    Signed-off-by: Mario A <marioanacl22@gmail.com>

--- networkbot/utils/keyboards.py
+++ networkbot/utils/keyboards.py
@@ -16,96 +16,86 @@
 # You should have received a copy of the GNU General Public License
 # along with tmnetbot.  If not, see <https://www.gnu.org/licenses/>.
 
-from typing import Iterable, Any, List
+from typing import Iterable, List, Tuple, Callable, Optional
 
-from pyrogram.types import InlineKeyboardButton as Button
+from pyrogram.types import Message, CallbackQuery, InlineKeyboardMarkup as Keyboard, InlineKeyboardButton as Button, \
+    User
 
-from .time import fix_minutes
+from .general import evaluate, args_joiner, extract_args
+from .. import filters as custom_filters
 from ..internationalization import translator
+from ..telegram import telegram
 
 
 _ = translator("general")
-
-_BUTTONS = ('1m', 1), ('15m', 15), ('1h', 60), ('4h', 240)
-
-
-def select_time_keyboard(prefix: str, time: int, args: Iterable[Any] = None, buttons=_BUTTONS):
-    if args is None:
-        args = []
-
-    time_keyboard = []
-    for label, value in buttons:
-        time_keyboard.append(select_time_row(label, value, prefix, time, args))
-
-    return time_keyboard
-
-
-def select_time_row(label: str, value: int, prefix: str, time: int, args: Iterable[Any] = None) -> List[Button]:
-    buttons_row: List[Button] = []
-
-    if args is None:
-        args = []
-
-    for i in range(2):
-        sign = i % 2
-
-        change = +value if sign == 0 else -value
-
-        buttons_row.append(custom_btn(f"{'+-'[sign]}{label}", prefix, [*args, max(0, change + time)]))
-
-    return buttons_row
-
-
-def select_double_time_keyboard(prefix: str, start: int, end: int, args: Iterable[Any] = None, buttons=_BUTTONS) -> \
-                                                                                                     List[List[Button]]:
-    if args is None:
-        args = []
-
-    start, end = map(fix_minutes, (start, end))
-
-    time_keyboard = []
-    for label, value in buttons:
-        time_keyboard.append(select_double_time_row(label, value, prefix, start, end, args))
-
-    return time_keyboard
-
-
-def select_double_time_header() -> List[Button]:
-    # TODO: "locale" param is missing
-    return [dummy_btn(_("from_time")), dummy_btn(_("to_time"))]
-
-
-def select_double_time_row(label: str, value: int, prefix: str, start: int, end: int, args: Iterable[Any] = None) \
-                                                                                                        -> List[Button]:
-    buttons_row: List[Button] = []
-
-    if args is None:
-        args = []
-
-    for i in range(4):
-        start_time, end_time = start, end
-        sign, side = i % 2, i // 2
-
-        change = +value if sign == 0 else -value
-
-        if side == 0:
-            start_time = fix_minutes(start + change)
-        else:
-            end_time = fix_minutes(end + change)
-
-        buttons_row.append(custom_btn(f"{'+-'[sign]}{label}", prefix, [*args, start_time, end_time]))
-
-    return buttons_row
-
-
-def custom_btn(label: str, prefix: str, args: Iterable[Any]) -> Button:
-    return Button(label, f"{'_'.join(map(str, [prefix, *args]))}")
 
 
 def dummy_btn(label: str) -> Button:
     return Button(label, "dummy")
 
 
-def confirm_btn(prefix: str, args: Iterable[Any]) -> Button:
-    # TODO: "locale" param is missing
-    return custom_btn(_("confirm"), prefix, args)
+def generate_pattern_buttons(*prefix, pattern: Iterable[Tuple[str, int]],
+                             btn_text: Callable[[str, str, User], str] = lambda sign, label, user: f"{sign}{label}") \
+        -> Callable[..., List[List[Button]]]:
+    def func(user: User, *args):
+        buttons_rows = []
+
+        for label, change in pattern:
+            row = []
+
+            for i, value in enumerate(args):
+                plus, minus = list(args), list(args)
+
+                plus[i] += change
+                minus[i] -= change
+
+                for sign, parameters in ('+', plus), ('-', minus):
+                    row.append(Button(btn_text(sign, label, user), args_joiner(*prefix, 'set', *parameters)))
+
+            buttons_rows.append(row)
+
+        return buttons_rows
+
+    return func
+
+
+def handle_double_keyboard(*prefix, defaults, text, columns_number: int = 1, map_fn: Callable[[int], int] = lambda n: n,
+                           buttons, buttons_before=None, buttons_after=None, on_confirm: Optional[Callable]) -> None:
+    _, _g = translator("settings"), translator("general")
+
+    buttons_pattern = generate_pattern_buttons(*prefix, pattern=buttons)
+
+    async def edit_message(message: Message, user: User, *args: int):
+        msg_text = evaluate(text, user, *args)
+
+        kb = Keyboard([
+            *(evaluate(buttons_before, user, *args) or []),
+            *(evaluate(buttons_pattern, user, *args) or []),
+            [Button(_g("confirm", locale=user.language_code), args_joiner(*prefix, "confirm", *args))],
+            *(evaluate(buttons_after, user, *args) or [])
+        ])
+
+        await message.edit_text(msg_text, reply_markup=kb)
+
+    @telegram.on_callback_query(custom_filters.arguments(*prefix, "initial"))
+    async def initial(__, callback_query: CallbackQuery):
+        await callback_query.answer()
+
+        await edit_message(callback_query.message, callback_query.from_user, *evaluate(defaults))
+
+    @telegram.on_callback_query(custom_filters.arguments(*prefix, "set"))
+    async def _set(__, callback_query: CallbackQuery):
+        await callback_query.answer()
+
+        args = map(map_fn, extract_args(callback_query.data, columns_number, int))
+
+        await edit_message(callback_query.message, callback_query.from_user, *args)
+
+    @telegram.on_callback_query(custom_filters.arguments(*prefix, "confirm"))
+    async def confirm(__, callback_query: CallbackQuery):
+        await callback_query.answer()
+
+        args = map(map_fn, extract_args(callback_query.data, columns_number, int))
+
+        if on_confirm is not None:
+            await on_confirm(callback_query.message, callback_query.from_user, *args)
